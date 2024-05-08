81e9dd7a8301ed5550569c3f98bf6b91920cff3d
BeyzaZngn <beyzazengin76@gmail.com>
2024-05-08 22:07:16 +0300

    day 20 created

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Beyza Zengin on 8.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
