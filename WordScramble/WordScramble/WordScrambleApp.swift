4fc9492d4bae9fb47f06efd65f8d91531d3668d0
BeyzaZngn <beyzazengin76@gmail.com>
2024-07-11 18:33:36 +0300

    WordScramble created.

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Beyza Zengin on 11.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
