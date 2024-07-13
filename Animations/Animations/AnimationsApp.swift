5591f0527ec061414711dcbfec7d5cc7e6d39d28
BeyzaZngn <beyzazengin76@gmail.com>
2024-07-13 17:11:20 +0300

    Animations created.

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Beyza Zengin on 13.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
