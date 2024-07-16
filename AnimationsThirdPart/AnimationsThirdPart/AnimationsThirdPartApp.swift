a96f141101101bf3caa29a06523cc4c05f14f4d2
BeyzaZngn <beyzazengin76@gmail.com>
2024-07-16 18:24:41 +0300

    third part

---
+++ AnimationsThirdPart/AnimationsThirdPart/AnimationsThirdPartApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsThirdPartApp.swift
+//  AnimationsThirdPart
+//
+//  Created by Beyza Zengin on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsThirdPartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationsThirdPart/AnimationsThirdPart/ContentView.swift
@@ -0,0 +1,90 @@
+//
+//  ContentView.swift
+//  AnimationsThirdPart
+//
+//  Created by Beyza Zengin on 15.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let letters = Array("Hello SwiftUI")
+
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    @State private var isShowingRed = false
+
+    var body: some View {
+        
+        VStack {
+            
+            Button("Tap Me") {
+                enabled.toggle()
+            }
+            .frame(width: 200, height: 200)
+            .background(enabled ? .blue : .red)
+            .animation(.default, value: enabled)
+            .foregroundStyle(.white)
+            .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+            .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
+            
+            Spacer()
+            
+            LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+                       .frame(width: 300, height: 200)
+                       .clipShape(.rect(cornerRadius: 10))
+                       .offset(dragAmount)
+                       .gesture(
+                           DragGesture()
+                               .onChanged { dragAmount = $0.translation }
+                            .onEnded { _ in
+                                withAnimation(.bouncy) {
+                                    dragAmount = .zero
+                                }
+                            }
+                       )
+            
+// .bouncy, SwiftUI için yerleşik animasyon seçeneklerinden biridir
+//ve hafifçe zıplayan bir yay üretir.
+
+            HStack(spacing: 0) {
+                        ForEach(0..<letters.count, id: \.self) { num in
+                            Text(String(letters[num]))
+                                .padding(5)
+                                .font(.title)
+                                .background(enabled ? .blue : .red)
+                                .offset(dragAmount)
+                                .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+                        }
+                    }
+                    .gesture(
+                        DragGesture()
+                            .onChanged { dragAmount = $0.translation }
+                            .onEnded { _ in
+                                dragAmount = .zero
+                                enabled.toggle()
+                            }
+                    )
+            
+            Button("Tap Me") {
+                isShowingRed.toggle()
+                }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+ 
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
